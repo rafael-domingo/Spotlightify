e6d3a18163adfff4034be551195341929ce53e4b
Rafael Domingo <38447983+rafael-domingo@users.noreply.github.com>
2022-11-27 22:35:01 -0600

    Obtain Spotify access token

--- server/spotify.js
+++ server/spotify.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const request = require('request');
 const querystring = require('querystring');
 const router = express.Router();
 
@@ -26,7 +27,37 @@ router.get('/callback', async (req, res) => {
     console.log('callback')
     console.log(req.query.code);
     console.log(req.query.state);
-    next();
+    const authOptions = {
+        url: 'https://accounts.spotify.com/api/token',
+        form: {
+            code: req.query.code,
+            redirect_uri: process.env.redirect_uri,
+            grant_type: 'authorization_code'
+        },
+        headers: {
+            'Authorization': 'Basic ' + (new Buffer.from(process.env.client_id + ':' + process.env.client_secret).toString('base64'))
+        },  
+        json: true
+    }
+
+    request.post(authOptions, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            var access_token = body.access_token;
+            var refresh_token = body.refresh_token;
+
+            var options = {
+                url: 'https://api.spotify.com/v1/me',
+                headers: {
+                    'Authorization': 'Bearer ' + access_token
+                },
+                json: true
+            }
+            request.get(options, function (error, response, body) {
+                console.log(body)
+            })
+        }
+    })
+    // next();
 })
 
 module.exports = router;
