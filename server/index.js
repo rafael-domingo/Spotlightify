c69337e9da41dad1c243ef5a217abb04178191f1
Rafael Domingo <38447983+rafael-domingo@users.noreply.github.com>
2022-12-17 13:36:26 -0600

    Setup for Heroku

--- server/index.js
+++ server/index.js
@@ -14,12 +14,21 @@ app.use(express.static("public"))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, '../spotlightify/build')))
+
 app.get('/', (req, res, next) => {
   console.log('hello');
   next()
 })
 app.use('/Spotify', spotify);
 app.use('/SpotifyPlayback', playback);
+
+
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '../spotlightify/build/index.html'))
+})
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
